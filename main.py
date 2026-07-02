7eb60d01db6ef471e87773b2de09f9d602dcefaa
Ritik Parajuli <ritikparajuli123@gmail.com>
2026-07-02 23:33:56 +0545

    fixed

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ app.add_middleware(
         "http://127.0.0.1:8080",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://aryan-school-management-frontend.vercel.app",  # ← ADD THIS
     ],
     allow_credentials=True,
     allow_methods=["*"],
